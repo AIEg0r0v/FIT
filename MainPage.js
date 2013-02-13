1bf804b9fb8e8079d00eefe17706f9eee54fbd84
Alexander <alex.egorov@outlook.com>
2013-02-13 21:16:45 +0100

    jQuery_added

---
+++ MainPage.js
@@ -0,0 +1,27 @@
+
+//Welcome to JS and jQuery world
+
+//toggle Inspections Content
+function toggleContent(content){
+	//insert check that the variable is div. Better if it is a proper div
+	content.fadeToggle("slow");
+};
+
+//On change Inspection class when expanded/collapsed.
+ function toggleInspectionClass(inspection){
+	//insert check that the variable is inspection class
+	inspection.toggleClass("InspectionItem InspectionItemExpanded");
+	toggleContent(inspection.children(".IIContent"));
+	toggleContent(inspection.children(".IIHeader").children(".IIContentCollapse"));
+ };
+ 
+$(document).ready(function(){
+
+	// $("div.InspectionItem").click(function(){
+		// toggleInspectionClass($(this));
+	// });
+	$("div.IIHeader").click(function(){
+		var inspection = $(this).parent(".InspectionItem, .InspectionItemExpanded")
+		toggleInspectionClass(inspection);
+	});
+});
