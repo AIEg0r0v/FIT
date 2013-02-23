0a5644c6e66d2cfd293e64ecb8efe349e1d51023
Alexander <alex.egorov@outlook.com>
2013-02-23 14:21:27 +0100

    Gathering-storm.
    dock for chat has a tendensy to dissaper with no obvious reason.
    It happen on hover event. Some times it just dissapers, some times it
    works as it should (dissapear only when mouse pointer leave the chat).
    
    Added many artifacts to handle them and rearrange.
    Artifact-onhover should be appended to Artifact, so we could produce
    less html code. Can be done by jquery.
    
    Also group choice at first time with inspection should be added.
    Labels to inspections to indicate process.
    Assigning\reassigning people to artifacts shall be done.
    
    Free space in the middle should be tackled.
    Modals - rethink. and they are still not fixed

--- MainPage.js
+++ MainPage.js
@@ -25,10 +25,23 @@ function slideUpProps(container){
 function slideDownProps(container){
 		var ArtifactProps = container.children(".Artifact-onhover");
 		ArtifactProps.slideDown("fast");
-	}
+};
+
+function toggleChatDock(){
+	$(".ChatInner").fadeToggle("slow");
+};
+function fadeInContent(content){
+	//insert check that the variable is div. Better if it is a proper div
+	content.fadeIn("slow");
+};
+function fadeOutContent(content){
+	//insert check that the variable is div. Better if it is a proper div
+	content.fadeOut("slow");
+};
 $(document).ready(function(){
 	
 	var propsTimeout;
+	var ChatShown = true;
 	$("div.ArtifactBody").click(function(){
 		$("#InspectionItemModal").modal('show');
 		
@@ -49,7 +62,44 @@ $(document).ready(function(){
 		window.clearTimeout(propsTimeout);
 		slideUpProps($(this));
 	});
-	
 
+	$(".ChatInner").hover(function(){
+		fadeInContent($(".Dock"));
+	},function(){
+		 if(ChatShown){
+			propsTimeout = window.setTimeout(fadeOutContent,2000,$(".Dock"));
+		 }
+	});
+
+	$(".Dock").hover(function(){
+		if(ChatShown){
+			window.clearTimeout(propsTimeout);
+		}
+	},function(){
+		if(ChatShown){
+			fadeOutContent($(".Dock"));	
+		}
+	});
+
+	$(".Dock").click(function(){
+		if(ChatShown){
+			
+			fadeOutContent($(".ChatInner"));
+			$(".ChatContainer").removeClass("span6");
+			$(".Dock").css('left','0px');
+			ChatShown = false;
+			
+			
+		}
+		else{
+			$(".ChatContainer").addClass("span6");
+			fadeInContent($(".ChatInner"));
+			$(".Dock").css('left','-20px');
+			ChatShown = true;
+			
+			
+		}
+		
+	});
 });
 
